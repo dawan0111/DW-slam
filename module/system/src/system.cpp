324b6652d1a24a2a076b400b976e5bae005e88bb
KDW <msd030428@gmail.com>
2023-12-22 17:10:12 +0900

    update debug condition

--- module/system/src/system.cpp
+++ module/system/src/system.cpp
@@ -10,29 +10,26 @@ System<Keypoint>::System(Extractor extractor, bool debug) : debug_(debug) {
 template <typename Keypoint>
 void System<Keypoint>::processNextFrame(cv::Mat &&left_image,
                                         cv::Mat &&right_image) {
+  std::vector<Keypoint> left_keypoint_vector =
+      extractor_->extractFeature(left_image, 0.0);
+
+  std::vector<Keypoint> right_keypoint_vector =
+      extractor_->extractFeature(right_image, 0.0);
+
   if (debug_) {
     cv::Mat merge_image;
-    cv::Mat left_feature_image;
-    cv::Mat right_feature_image;
-
-    std::vector<Keypoint> left_keypoint_vector =
-        extractor_->extractFeature(left_image, 0.0);
 
     for (auto &keypoint : left_keypoint_vector) {
       dw_slam::extractor::Point point = keypoint.getXY();
-      cv::circle(left_image, cv::Point(point.x, point.y), 5,
+      cv::circle(left_image, cv::Point(point.x, point.y), 3,
                  cv::Scalar(0, 255, 0), -1);
     }
-
-    std::vector<Keypoint> right_keypoint_vector =
-        extractor_->extractFeature(right_image, 0.0);
 
     for (auto &keypoint : right_keypoint_vector) {
       dw_slam::extractor::Point point = keypoint.getXY();
-      cv::circle(right_image, cv::Point(point.x, point.y), 5,
+      cv::circle(right_image, cv::Point(point.x, point.y), 3,
                  cv::Scalar(0, 255, 0), -1);
     }
-    // cv::drawKeypoints(left_image, keypoint)
     cv::hconcat(left_image, right_image, merge_image);
 
     cv::imshow("original_image", merge_image);
