b98cfd16bd245a1564a09a13a0cb3c6f876552ef
KDW <msd030428@gmail.com>
2023-12-29 15:34:14 +0900

    add tracking and linking

---
+++ module/tracking/include/tracking/tracking.hpp
@@ -0,0 +1,13 @@
+#ifndef SLAM__TRACKING_HPP_
+#define SLAM__TRACKING_HPP_
+#include "type/camera.hpp"
+#include <iostream>
+#include <memory>
+
+namespace dw_slam::tracking {
+class Tracking {
+public:
+  Tracking();
+};
+} // namespace dw_slam::tracking
+#endif // SLAM__TRACKING_HPP_
---
+++ module/tracking/src/tracking.cpp
@@ -0,0 +1,5 @@
+#include "tracking/tracking.hpp"
+
+namespace dw_slam::tracking {
+Tracking::Tracking() { std::cout << "===== Tracking =====" << std::endl; }
+} // namespace dw_slam::tracking
